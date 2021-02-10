e413e92c28022fbbfc03fe0a2f4c6db96f05d3c6
roman <roman@compuweigh.com>
2021-02-10 10:00:18 -0500

    Problem 206. Reverse a Linked List solutions: iterative, recursive, and dummy node

---
+++ reverse-a-linked-list.js
@@ -0,0 +1,98 @@
+class Node {
+  constructor(val, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insert(val, next = null) {
+    let newNode = new Node(val, next);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;;
+    }
+  }
+
+  reverseIterativePrev() {
+    if (this.head === null) return this.head;
+    let previous = null;
+    let current = this.head;
+    let next = this.head.next;
+
+    while (current!== null && next!== null) {
+      current.next = previous;
+
+      previous = current;
+      current = next;
+      next = next.next;
+    }
+    current.next = previous;
+    this.head = current;
+    return this.head
+  }
+
+  reverseDummy() {
+    if (this.head === null) return this.head;
+    let dummy = new Node();
+
+    let current = this.head;
+    let next = current.next;
+
+    while (current !== null && next !== null) {
+      current.next = dummy.next;
+
+      dummy.next = current;
+      current = next;
+      next = next.next;
+    }
+    current.next = dummy.next;
+    this.head = current;;
+
+    return this.head;
+  }
+
+  reverseRecursive() {
+    const reverse = (previous, current, next) => {
+      if (current === null && next === null) {
+        this.head = previous;
+        return this.head;
+      }
+
+      current.next = previous;
+      previous = current;
+      current = next;
+      next = (next === null) ? next : next.next;
+
+      return reverse(previous, current, next)
+    }
+
+    if (this.head === null) return this.head;
+    let previous = null;
+    let current = this.head;
+    let next = this.head.next;
+    return reverse(previous, current, next)
+  }
+}
+
+let ll1 = new LinkedList();
+ll1.insert(1);
+ll1.insert(2);
+ll1.insert(3);
+ll1.insert(4);
+ll1.insert(5);
+
+// let result = ll1.reverseIterativePrev();
+let result = ll1.reverseDummy();
+let result2 = ll1.reverseRecursive();
+console.log(result)
+
