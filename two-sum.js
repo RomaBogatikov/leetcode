c1f8f163c2fc156f43ed50195f805b7bbfef3d0d
RomanBogatikov <ivaroman2000@gmail.com>
2021-02-19 08:15:18 -0500

    Problem 1.Two Sum solution

---
+++ two-sum.js
@@ -0,0 +1,56 @@
+// Problem 1. Two Sum
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+
+var twoSumTwoPass = function(nums, target) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    map[nums[i]] = i;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    if (map.hasOwnProperty(complement) && map[complement] !== i) {
+      return [i, map[complement]]
+    }
+  }
+  return [];
+}
+
+var twoSumOnePass = function(nums, target) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    console.log(map[complement])
+    if (map.hasOwnProperty(complement) && map[complement] !== i) {
+      return [i, map[complement]];
+    }
+    map[nums[i]] = i;
+  }
+  return [];
+}
+
+let arr = [3, 2, 3]
+let result = twoSumOnePass(arr, 6);
+console.log(result)
