01856571608816780b81cf5859b55072bce7b80e
RomanBogatikov <ivaroman2000@gmail.com>
2021-03-07 13:08:16 -0500

    group anagrams solution that times out

--- group-anagrams.js
+++ group-anagrams.js
@@ -7,21 +7,22 @@ var groupAnagrams = function(strs) {
     let hash2 = {};
 
     for (let i = 0; i < str1.length; i++) {
-      if (!hash1.hasOwnProperty(str1[i])) {
-        hash1[str1[i]] = 0;
+      if (!hash1[str1[i]]) {
+        hash1[str1[i]] = 1;
       } else {
         hash1[str1[i]]++;
       }
     }
     for (let i = 0; i < str2.length; i++) {
-      if (!hash2.hasOwnProperty(str2[i])) {
-        hash2[str2[i]] = 0;
+      if (!hash2[str2[i]]) {
+        hash2[str2[i]] = 1;
       } else {
         hash2[str2[i]]++;
       }
     }
 
-    return Object.keys(hash1).every(key => hash2.hasOwnProperty(key) && hash2[key] === hash1[key])
+    if (Object.keys(hash1).length !== Object.keys(hash2).length) return false;
+    return Object.keys(hash1).every(key => hash2[key] === hash1[key]);
   }
 
   let hashMap = {};
@@ -41,10 +42,37 @@ var groupAnagrams = function(strs) {
       }
   }
 
-  return Object.values(hashMap)
+  return Object.values(hashMap);
 };
 
 let arr = ['ac', 'c']
 let result = groupAnagrams(arr);
 console.log(result)
 // console.log(isAnagram("ac","c"))
+
+
+// let isAnagram = (str1, str2) => {
+  // if (str1 === '' && str2 === '') return true;
+  // if (str1 === '' || str2 === '') return false;
+//   let hash1 = {};
+//   let hash2 = {};
+
+//   for (let i = 0; i < str1.length; i++) {
+//     if (!hash1[str1[i]]) {
+//       hash1[str1[i]] = 1;
+//     } else {
+//       hash1[str1[i]]++;
+//     }
+//   }
+//   for (let i = 0; i < str2.length; i++) {
+//     if (!hash2[str2[i]]) {
+//       hash2[str2[i]] = 1;
+//     } else {
+//       hash2[str2[i]]++;
+//     }
+//   }
+
+//   return Object.keys(hash1).every(key => hash2[key] === hash1[key])
+// }
+
+//  console.log(isAnagram('abbb', 'aaab'))
