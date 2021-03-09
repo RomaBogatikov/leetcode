33e0977b9b3d1c06afc7fd233cf49884d1287187
RomanBogatikov <ivaroman2000@gmail.com>
2021-03-09 10:55:49 -0500

    Problem 938.Range Sum of BST solution

--- to-lower-case.js
+++ to-lower-case.js
@@ -16,7 +16,7 @@
 // Input: "LOVELY"
 // Output: "lovely"
 
-
+// my solution
 var toLowerCase = function(str) {
   let result = '';
   let code;
@@ -30,3 +30,18 @@ var toLowerCase = function(str) {
   }
   return result;
 };
+
+// a better solution without magic numbers
+
+var toLowerCase = function(str) {
+  let result = '';
+  let diff = Math.abs('a'.charCodeAt(0) - 'A'.charCodeAt(0));
+  for (let i  = 0; i < str.length; i++) {
+    if (str[i] >= 'A' && str[i] <= 'Z') {
+      result += String.fromCharCode(str[i].charCodeAt(0) + diff);
+    } else {
+      result += str[i]
+    }
+  }
+  return result;
+}
