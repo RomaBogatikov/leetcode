48c4c8d1a05221d32b681485b8d147ed3f030a71
RomanBogatikov <ivaroman2000@gmail.com>
2021-03-25 17:35:53 -0400

    Problem 977.Squares of a Sorted Array solution

---
+++ squares-of-sorted-array.js
@@ -0,0 +1,65 @@
+// 977. Squares of a Sorted Array
+// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+
+
+// Example 1:
+
+// Input: nums = [-4,-1,0,3,10]
+// Output: [0,1,9,16,100]
+// Explanation: After squaring, the array becomes [16,1,0,9,100].
+// After sorting, it becomes [0,1,9,16,100].
+// Example 2:
+
+// Input: nums = [-7,-3,2,3,11]
+// Output: [4,9,9,49,121]
+
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -104 <= nums[i] <= 104
+// nums is sorted in non-decreasing order.
+
+
+// Follow up: Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach?
+
+var sortedSquares = function(nums) {
+  let result = [];
+  let fromNums;
+  while (nums.length) {
+      if (Math.abs(nums[0]) > Math.abs(nums[nums.length - 1])) {
+          fromNums = nums.shift();
+      } else {
+          fromNums = nums.pop();
+      }
+
+      let squared = fromNums**2;
+      result.unshift(squared);
+  }
+
+  return result;
+};
+
+var sortedSquares2 = function(nums) {
+  let result = [];
+  let fromNums;
+  let left = 0;
+  let right = nums.length - 1;
+  let count = nums.length - 1;
+  while (left <= right) {
+      if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+          fromNums = nums[left];
+          left++;
+      } else {
+          fromNums = nums[right];
+          right--;
+      }
+
+      let squared = fromNums**2;
+      result[count] = squared;
+      count--;
+  }
+
+  return result;
+};
