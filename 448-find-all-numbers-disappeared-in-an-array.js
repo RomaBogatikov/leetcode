e18f2c7ee296605aee193d216b1d30d545c534d2
RomanBogatikov <ivaroman2000@gmail.com>
2021-04-08 08:54:53 -0400

    Problem 448. Find All Numbers Disappeared in an Array solution

---
+++ 448-find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,31 @@
+// 448. Find All Numbers Disappeared in an Array
+
+// Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+
+
+// Example 1:
+
+// Input: nums = [4,3,2,7,8,2,3,1]
+// Output: [5,6]
+// Example 2:
+
+// Input: nums = [1,1]
+// Output: [2]
+
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 105
+// 1 <= nums[i] <= n
+
+var findDisappearedNumbers = function(nums) {
+  let set = new Set(nums);
+  let result = [];
+  for (let i = 1; i <= nums.length; i++) {
+      if (!set.has(i)) result.push(i);
+  }
+
+  return result;
+};
