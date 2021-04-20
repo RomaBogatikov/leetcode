99b8bd33a8a9d40dced44cb004a0935eedb350f7
RomanBogatikov <ivaroman2000@gmail.com>
2021-04-20 10:02:18 -0400

    Problem 976.Largest Perimeter Triangle solution

---
+++ 976-largest-perimeter-triangle.js
@@ -0,0 +1,39 @@
+// 976. Largest Perimeter Triangle
+
+
+// Given an integer array nums, return the largest perimeter of a triangle with a non-zero area, formed from three of these lengths. If it is impossible to form any triangle of a non-zero area, return 0.
+
+
+
+// Example 1:
+
+// Input: nums = [2,1,2]
+// Output: 5
+// Example 2:
+
+// Input: nums = [1,2,1]
+// Output: 0
+// Example 3:
+
+// Input: nums = [3,2,3,4]
+// Output: 10
+// Example 4:
+
+// Input: nums = [3,6,2,3]
+// Output: 8
+
+
+// Constraints:
+
+// 3 <= nums.length <= 104
+// 1 <= nums[i] <= 106
+
+var largestPerimeter = function(nums) {
+  if (nums.length < 3) return 0;
+  let sortedNums = nums.sort((a, b) => b - a);
+
+  for (let i = 2; i < sortedNums.length; i++) {
+      if (sortedNums[i-2] < sortedNums[i-1] + sortedNums[i]) return sortedNums[i] + sortedNums[i-1] + sortedNums[i-2];
+  }
+  return 0;
+};
